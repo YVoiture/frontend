c90b2323b42ef54d794ab381646f9cabed9260bd
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-04-12 16:23:14 +0200

    feat: add navlinks in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,6 +31,37 @@ function Sidebar({ isDrawerOpen }) {
                         </NavLink>
                     </li>
                 </ul>
+                <div className="pt-5 pl-2 mt-5 mb-4 text-sm font-medium text-gray-500 uppercase border-t border-gray-200 dark:text-gray-400 dark:border-gray-700">
+                    Administration
+                </div>
+                <ul className="space-y-2">
+                    <li>
+                        <NavLink
+                                to="/users"
+                                className={({ isActive }) =>
+                                    `flex items-center p-2 text-base font-medium rounded-lg
+                                    ${isActive ? 'bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white' : 'text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700'}`
+                                }
+                            >
+                            <svg aria-hidden="true" className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path></svg>
+                            <span className="ml-3">Utilisateurs</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                                to="/roles"
+                                className={({ isActive }) =>
+                                    `flex items-center p-2 text-base font-medium rounded-lg
+                                    ${isActive ? 'bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white' : 'text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700'}`
+                                }
+                            >
+                            <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fillRule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clipRule="evenodd"/>
+                            </svg>
+                            <span className="ml-3">Rôles</span>
+                        </NavLink>
+                    </li>
+                </ul>
             </div>
         </aside>
     );
