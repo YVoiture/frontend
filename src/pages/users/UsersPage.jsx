7991636f7f7d6f799c3d03f659dc7a7c5c8bdb12
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-07-02 11:06:52 +0200

    feat: add api call on users

--- src/pages/users/UsersPage.jsx
+++ src/pages/users/UsersPage.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import Table from "../../components/Table";
 
-function UsersPage() {
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
+const UsersPage = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch(`${BACKEND_URL}/users`);
+                const data = await response.json();
+                
+                const filteredData = data.map(user => ({
+                    last_name: user.lastName,
+                    first_name: user.firstName,
+                    email: user.email,
+                    role: user.role.name
+                }));
+
+                setUsers(filteredData);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    })
+
     return (
         <>
             <h1 className="text-2xl font-medium">Liste des utilisateurs</h1>
-            <Table dataName="un utilisateur" columns={["Nom", "Prénom", "Adresse e-mail", "Rôle"]} />
+            <Table dataName="un utilisateur" columns={["Nom", "Prénom", "Adresse e-mail", "Rôle"]} dataList={users} />
         </>
     )
 }
