623ecf1fd9650b5ced9015a4df5a98d8298a2d67
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-07-02 11:52:58 +0200

    feat: add api call on suppliers

--- src/pages/suppliers/SuppliersPage.jsx
+++ src/pages/suppliers/SuppliersPage.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
 import Table from "../../components/Table";
 
-function SupliersPage() {
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const SupliersPage = () => {
+    const [suppliers, setSuppliers] = useState([]);
+
+    useEffect(() => {
+        const fetchSuppliers = async () => {
+            try {
+                const response = await fetch(`${BACKEND_URL}/suppliers`);
+                const data = await response.json();
+                
+                const filteredData = data.map(supplier => ({
+                    name: supplier.name,
+                    type: supplier.type,
+                    address: supplier.adress,
+                    email: supplier.email,
+                    phone: '0' + supplier.number
+                }));
+
+                setSuppliers(filteredData);
+            } catch (error) {
+                console.error('Error fetching suppliers:', error);
+            }
+        };
+
+        fetchSuppliers();
+    })
+
     return (
         <>
             <h1 className="text-2xl font-medium">Liste des fournisseurs</h1>
-            <Table dataName="un fournisseur" columns={["Nom", "Type", "Adresse postale", "Adresse e-mail", "Numéro de téléphone"]} />
+            <Table dataName="un fournisseur" columns={["Nom", "Type", "Adresse postale", "Adresse e-mail", "Numéro de téléphone"]} dataList={suppliers} />
         </>
     );
 }
