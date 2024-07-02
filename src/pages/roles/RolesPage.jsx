d2d6baebe2b7afa9270ad67cce77c2b56e8b3acc
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-07-02 09:59:21 +0200

    feat: add api call on roles

--- src/pages/roles/RolesPage.jsx
+++ src/pages/roles/RolesPage.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import Table from "../../components/Table";
 
-function RolesPage() {
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
+const RolesPage = () => {
+    const [roles, setRoles] = useState([]);
+
+    useEffect(() => {
+        const fetchRoles = async () => {
+            try {
+                const response = await fetch(`${BACKEND_URL}/roles`);
+                const data = await response.json();
+                
+                const filteredData = data.map(role => ({
+                    name: role.name,
+                    position: role.position
+                }));
+
+                setRoles(filteredData);
+            } catch (error) {
+                console.error('Error fetching roles:', error);
+            }
+        };
+
+        fetchRoles();
+    }, [])
+
     return (
         <>
             <h1 className="text-2xl font-medium">Liste des rôles</h1>
-            <Table dataName="un rôle" columns={["Nom", "Position"]} />
+            <Table dataName="un rôle" columns={["Nom", "Position"]} dataList={roles} />
         </>
     );
 }
