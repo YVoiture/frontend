871a3f6c5cbcb1539ac79cc00bc00b15c5209334
DevLudo <roux.ludovic.pro@gmail.com>
2024-07-05 03:10:43 +0200

    Update CreateMaintenance.jsx

--- src/pages/maintenances/CreateMaintenance.jsx
+++ src/pages/maintenances/CreateMaintenance.jsx
@@ -2,18 +2,37 @@ import useSubmit from "../../hooks/useSubmit";
 import FormLayout from "../../components/FormLayout";
 import TextInput from "../../components/TextInput";
 import SelectInput from "../../components/SelectInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CreateMaintenance = () => {
     const [initialFormState, setInitialFormState] = useState({
         startDate: '',
         endDate: '',
         type: '',
+        car: '',
     })
     const [formState, handleChange, handleSubmit] = useSubmit("maintenances", initialFormState);
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        const fetchCars = async () => {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/cars`);
+                const data = await response.json();
+                setCars(data);
+            } catch (error) {
+                console.error('Error fetching cars data:', error);
+            }
+        };
+
+        fetchCars();
+    }, []);
+
+    const carOptions = cars.map(car => ({ value: car.id, label: `${car.brand} ${car.model}` }));
 
     return (
         <FormLayout title="Ajouter une maintenance" onSubmit={handleSubmit}>
+            <SelectInput label="Voiture" name="car" value={formState.car} onChange={handleChange} options={carOptions} />
             <TextInput label="Date de début" name="startDate" type="date" value={formState.startDate} onChange={handleChange} />
             <TextInput label="Date de fin" name="endDate" type="date" value={formState.endDate} onChange={handleChange} />
             <SelectInput label="Type" name="type" value={formState.type} onChange={handleChange} options={[
